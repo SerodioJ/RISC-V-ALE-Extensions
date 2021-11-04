8abce7243e31529329f9a4177a862438c90c2028
Antonio Guimaraes <antonio.guimaraes@ic.unicamp.br>
2021-11-04 02:12:50 -0300

    Update canvas

--- devices/canvas.js
+++ devices/canvas.js
@@ -4,19 +4,62 @@ import {Device} from "./utils.js";
 class Canvas extends Device{
   setup(){
     this.addTab("Canvas", "fa-paint-brush", "canvas_device", `
-      <canvas id="canvas_device_canvas" style="width: 100%;height: 100%;"></canvas>
+    <div class="modal fade" role="dialog" tabindex="-1" id="modal_canvas" data-keyboard="false" data-backdrop="static">
+      <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">Canvas</h4><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+          </div>
+          <div class="modal-body" style="overflow-x: auto;">
+            <canvas id="canvas_device_canvas"></canvas>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--  <div class="form-group">
+    <label for="canvas_scale_x">Scale X</label>
+    <input type="text" class="form-control" name="canvas_scale_x" id="canvas_scale_x" placeholder="" value=1>
+    </div>
+    <div class="form-group">
+    <label for="canvas_scale_y">Scale Y</label>
+    <input type="text" class="form-control" name="canvas_scale_y" id="canvas_scale_y" placeholder="" value=1>
+    </div> --> 
+    <div class="form-group">
+    <label for="canvas_size_x">Width:</label>
+    <input type="text" class="form-control" name="canvas_size_x" id="canvas_size_x" placeholder="" value=512>
+    </div>
+    <div class="form-group">
+    <label for="canvas_size_y">Height:</label>
+    <input type="text" class="form-control" name="canvas_size_y" id="canvas_size_y" placeholder="" value=512>
+    </div>
+    <button type="button" class="btn btn-primary" id="canvas_reset">Reset Canvas</button>
+    <button type="button" class="btn btn-primary" id="canvas_open_canvas">Open Canvas</button>
     `);
+
+    document.getElementById("canvas_open_canvas").onclick = _ => {
+      if(!($("#modal_canvas").data('bs.modal') || {})._isShown){
+        $('#modal_canvas').modal({backdrop: false,show: true});
+        $('#modal_canvas').draggable({handle: ".modal-header"});
+      }
+    }
+
+    document.getElementById("canvas_reset").onclick = _ => {
+      // this.ctx.scale(document.getElementById("canvas_scale_x").value, 
+      //                document.getElementById("canvas_scale_y").value);
+      this.imageData = this.ctx.createImageData(document.getElementById("canvas_size_x").value, 
+                                                document.getElementById("canvas_size_y").value);
+      this.width = parseInt(document.getElementById("canvas_size_x").value);
+    }
+
 
     this.canvas = document.getElementById("canvas_device_canvas");
     this.ctx = this.canvas.getContext('2d');
     this.imageData = this.ctx.createImageData(512, 512);
+    this.canvas.width = 512;
+    this.canvas.height = 512;
     this.width = 512;
-    this.scalex = 1;
-    this.scaley = 1;
 
     setInterval(_=>{
-      this.canvas.width  = this.canvas.offsetWidth/this.scalex;
-      this.canvas.height = this.canvas.offsetHeight/this.scaley;
       this.ctx.putImageData(this.imageData, 0, 0);
     }, 100);
     var syscall =`
@@ -34,9 +77,18 @@ class Canvas extends Device{
       this.imageData = this.ctx.createImageData(reg.a0, reg.a1);
     }.bind(this));
     this.registerSyscall(2202, "Canvas setScaling", syscall, function (reg) {
-      this.scalex = reg.a0;
-      this.scaley = reg.a1;
+      this.ctx.scale(reg.a0, reg.a1);
     }.bind(this));
+
+    this.bus.watchAddress(this.base_addr, function (value) {
+      const size = this.bus.mmio.load(this.base_addr + 2, 2);
+      const start = this.bus.mmio.load(this.base_addr + 4, 4);
+      for (let i = 0; i < size; i++) {
+        const element = this.bus.mmio.load(this.base_addr + 8 + i, 1);
+        this.imageData.data[start + i] = element;
+      }
+      this.bus.mmio.store(this.base_addr, 1, 0);
+    }.bind(this), 1, 1);
   }
 }
 
